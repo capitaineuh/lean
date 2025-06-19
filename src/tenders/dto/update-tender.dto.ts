4e21d56ef806601508f4d38022022608f3290352
allexsi <a_pichon8@stu-digital-campus.fr>
2025-06-19 13:22:44 +0200

    fusion

--- src/tenders/dto/update-tender.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTenderDto } from './create-tender.dto';
-
-export class UpdateTenderDto extends PartialType(CreateTenderDto) {} 
--- src/tenders/schemas/tender.schema/tender.schema.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TenderSchema } from './tender.schema';
-
-describe('TenderSchema', () => {
-  it('should be defined', () => {
-    expect(new TenderSchema()).toBeDefined();
-  });
-});
